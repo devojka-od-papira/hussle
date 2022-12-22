7e11b6644ce253c1fa884478889bff80a5b63b0f
Bojana Bojic <bojic@work.co>
2022-12-22 10:58:40 +0100

    Refactor button stories

--- src/components/button/Button.stories.tsx
+++ src/components/button/Button.stories.tsx
@@ -13,9 +13,6 @@ export default {
   title: 'Component/Button',
   component: Button,
   argTypes: {
-    className: {
-      type: 'string',
-    },
     onClick: {
       type: 'function',
     },
@@ -25,44 +22,52 @@ export default {
   },
 } as ComponentMeta<typeof Button>;
 
-const Template: ComponentStory<typeof Button> = (args) => (
-  <Button {...args}>
+const TemplateTab: ComponentStory<typeof Button> = (args) => (
+  <Button {...args} className={cx(styles.buttonRed, styles.button)}>
+    Sign in
+  </Button>
+);
+
+const TemplateSignInButton: ComponentStory<typeof Button> = (args) => (
+  <Button {...args} className={cx(styles.buttonColor, styles.button)}>
     Sign in
   </Button>
 );
 
 const TemplateIcon: ComponentStory<typeof Button> = (args) => (
-  <Button {...args}>
+  <Button {...args} className={cx(styles.buttonIcon, styles.button)}>
     <Paperclip color="gray" size={16} />
   </Button>
 );
 
 const TemplateIconCircle: ComponentStory<typeof Button> = (args) => (
-  <Button {...args}>
+  <Button {...args} className={cx(styles.buttonPlusCircle, styles.button)}>
     <Plus color="white" size={20} />
   </Button>
 );
 
 const TemplateIconShadow: ComponentStory<typeof Button> = (args) => (
-  <Button {...args}>
+  <Button {...args} className={cx(styles.buttonShadow, styles.button)}>
     <ChevronDown color="gray" size={9} />
   </Button>
 );
 
 const TemplateIconDotted: ComponentStory<typeof Button> = (args) => (
-  <Button {...args}>
+  <Button {...args} className={cx(styles.buttonDotted, styles.button)}>
     <Plus color="gray" size={9} />
   </Button>
 );
 
 const TemplateTextIcon: ComponentStory<typeof Button> = (args) => (
-  <Button {...args}>
+  <Button {...args} className={cx(styles.textIcon, styles.button)}>
     Add task
     <PlusCircle color="gray" size={16} />
   </Button>
 );
 
-export const button = Template.bind({});
+export const buttonTab = TemplateTab.bind({});
+
+export const buttonTabColor = TemplateSignInButton.bind({});
 
 export const buttonIcon = TemplateIcon.bind({});
 
@@ -73,27 +78,3 @@ export const buttonIconShadow = TemplateIconShadow.bind({});
 export const buttonIconDotted = TemplateIconDotted.bind({});
 
 export const buttonTextIcon = TemplateTextIcon.bind({});
-
-button.args = {
-  className: cx(styles.buttonRed, styles.button),
-};
-
-buttonIcon.args = {
-  className: cx(styles.buttonIcon, styles.button),
-};
-
-buttonIconCircle.args = {
-  className: cx(styles.buttonPlusCircle, styles.button),
-};
-
-buttonIconShadow.args = {
-  className: cx(styles.buttonShadow, styles.button),
-};
-
-buttonIconDotted.args = {
-  className: cx(styles.buttonDotted, styles.button),
-};
-
-buttonTextIcon.args = {
-  className: cx(styles.textIcon, styles.button),
-};
