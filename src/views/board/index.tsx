9982bf80c9f16f5afb5e1c3024f7efa93e869891
Bojana Bojic <76129338+devojka-od-papira@users.noreply.github.com>
2023-01-26 01:38:31 +0100

    Create UI board view (#12)
    * Create board
    
    
    Co-authored-by: Bojana Bojic <bojic@work.co>

--- src/views/board/index.tsx
+++ src/views/board/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import cx from 'classnames';
-import { User } from 'react-feather';
+import { Plus, User } from 'react-feather';
 import Header from '../../components/header';
 import Sidebar from '../../components/sidebar';
 import Typography, { TextType } from '../../components/typography';
 import Button from '../../components/button';
+import Column from '../../components/column';
+import { Priority } from '../../constants';
 
 import styles from './Board.module.scss';
 
@@ -13,6 +15,156 @@ const Board = () => {
     console.log('user');
   };
 
+  const cardData = [
+    {
+      priority: Priority.LOW,
+      description: 'Company website redesign.',
+      attachmentNumber: 3,
+      commentNumber: 1,
+    },
+    {
+      priority: Priority.MED,
+      description: 'Company website redesign.',
+      attachmentNumber: 2,
+      commentNumber: 3,
+    },
+    {
+      priority: Priority.HIGH,
+      description: 'Company website redesign.',
+      attachmentNumber: 1,
+      commentNumber: 2,
+    },
+  ];
+
+  const board = [
+    {
+      title: 'Backlog',
+      id: 0,
+      tasks: [
+        {
+          priority: Priority.LOW,
+          description: 'Company website redesign.',
+          attachmentNumber: 3,
+          commentNumber: 1,
+        },
+        {
+          priority: Priority.MED,
+          description: 'Company website redesign.',
+          attachmentNumber: 2,
+          commentNumber: 3,
+        },
+        {
+          priority: Priority.HIGH,
+          description: 'Company website redesign.',
+          attachmentNumber: 1,
+          commentNumber: 2,
+        },
+      ],
+    },
+    {
+      title: 'In Progess',
+      id: 1,
+      tasks: [
+        {
+          priority: Priority.LOW,
+          description: 'Company website redesign.',
+          attachmentNumber: 3,
+          commentNumber: 1,
+        },
+        {
+          priority: Priority.MED,
+          description: 'Company website redesign.',
+          attachmentNumber: 2,
+          commentNumber: 3,
+        },
+        {
+          priority: Priority.HIGH,
+          description: 'Company website redesign.',
+          attachmentNumber: 1,
+          commentNumber: 2,
+        },
+      ],
+    },
+    {
+      title: 'Review',
+      id: 2,
+      tasks: [
+        {
+          priority: Priority.LOW,
+          description: 'Company website redesign.',
+          attachmentNumber: 3,
+          commentNumber: 1,
+        },
+        {
+          priority: Priority.MED,
+          description: 'Company website redesign.',
+          attachmentNumber: 2,
+          commentNumber: 3,
+        },
+        {
+          priority: Priority.HIGH,
+          description: 'Company website redesign.',
+          attachmentNumber: 1,
+          commentNumber: 2,
+        },
+        {
+          priority: Priority.MED,
+          description: 'Company website redesign.',
+          attachmentNumber: 1,
+          commentNumber: 2,
+        },
+      ],
+    },
+    {
+      title: 'Complete',
+      id: 3,
+      tasks: [
+        {
+          priority: Priority.LOW,
+          description: 'Company website redesign.',
+          attachmentNumber: 3,
+          commentNumber: 1,
+        },
+        {
+          priority: Priority.MED,
+          description: 'Company website redesign.',
+          attachmentNumber: 2,
+          commentNumber: 3,
+        },
+        {
+          priority: Priority.HIGH,
+          description: 'Company website redesign.',
+          attachmentNumber: 1,
+          commentNumber: 2,
+        },
+        {
+          priority: Priority.HIGH,
+          description: 'Company website redesign.',
+          attachmentNumber: 1,
+          commentNumber: 2,
+        },
+        {
+          priority: Priority.MED,
+          description: 'Company website redesign.',
+          attachmentNumber: 1,
+          commentNumber: 2,
+        },
+        {
+          priority: Priority.HIGH,
+          description: 'Company website redesign.',
+          attachmentNumber: 1,
+          commentNumber: 2,
+        },
+        {
+          priority: Priority.MED,
+          description: 'Company website redesign.',
+          attachmentNumber: 1,
+          commentNumber: 2,
+        },
+      ],
+    },
+  ];
+
   return (
     <div className={styles.container}>
       <Header />
@@ -20,11 +172,22 @@ const Board = () => {
       <div className={styles.wrapper}>
         <div className={styles.inner}>
           <Typography variant="h1" type={TextType.Heading1}>Studio Board</Typography>
-          <Button onClick={handleClick} className={cx(styles.buttonCircle, styles.button)}>
-            <User color="white" size={20} />
-          </Button>
+          <div className={styles.buttonWrapper}>
+            <Button onClick={handleClick} className={cx(styles.buttonCircle)}>
+              <User color="white" size={20} />
+            </Button>
+            <Button onClick={handleClick} className={cx(styles.buttonDotted)}>
+              <Plus color="gray" size={20} />
+            </Button>
+          </div>
         </div>
-        <div className={styles.columnWrapper} />
+        <div className={styles.columnsWrapper}>
+          {board.map((column) => (
+            <div key={column.id} className={styles.columnWrapper}>
+              <Column cardData={column.tasks} title={column.title} />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
