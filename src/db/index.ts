8511a6b52f8d528b07e40f12169f2585af62e62c
Bojana Bojic <bojic@work.co>
2023-01-27 00:17:52 +0100

    Clean up

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
-console.log(process.env.REACT_APP_API_KEY);
-
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
